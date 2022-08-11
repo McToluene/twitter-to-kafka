2973d2f74638209e62b8b9e66788070c7964ea79
McToluene <xclusiveniggaz10@live.com>
2022-08-11 22:14:05 +0100

    Twitter4j implementation added

---
+++ twitter-to-kafka-service/src/test/java/com/mctoluene/microservice/demo/twitter/to/kafka/service/TwitterToKafkaServiceApplicationTest.java
@@ -0,0 +1,12 @@
+package com.mctoluene.microservice.demo.twitter.to.kafka.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceApplicationTest {
+
+    @Test()
+    void loadContext() {
+    }
+}
